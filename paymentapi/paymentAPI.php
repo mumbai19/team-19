b97c212f4141411c85423d5791267087c6b9befe
Necil Dabre <dabre.necil@gmail.com>
2019-07-20 21:09:21 +0530

    v2 Ready Payment API

--- paymentapi/paymentAPI.php
+++ paymentapi/paymentAPI.php
@@ -1,5 +1,5 @@
 <?php 
-    include(DBConnection.php);
+    include('../config.php');
     $json = file_get_contents('php://input');
     
     $obj = json_decode($json);
@@ -7,14 +7,22 @@
     {
         $apikey = $obj->{'apikey'};
         $username = $obj->{'apikey'};
-        $username = $obj->{'apikey'};
-        $username = $obj->{'apikey'};
+        $amount = $obj->{'apikey'};
+        $transactionid = $obj->{'apikey'};
 
         $sql = "SELECT * FROM apikeys WHERE apikey = '$apikey'";
+        if($result = mysqli_query($link, $sql)){
+            if(mysqli_num_rows($result) == 1){
+                $sqlinsert = "INSERT INTO transactions VALUES(?, ?, ?, ?)";
+                if(mysqli_query($sqlinsert))
+                {
+                    $response = array("message"=>"successful");
+                }
+            }
+        }    
 
 
-
-
+        echo json_encode($response);
 
         
     }
