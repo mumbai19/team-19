d15567ea98b522d4b62c60f5fdc97e70a114eb1c
Chirag55 <chiragrohra5@gmail.com>
2019-07-21 05:11:32 +0530

    visualisation file basic

--- changeorder.php
+++ changeorder.php
@@ -5,11 +5,11 @@ if(isset($_POST['submit']))
 {
     $quan=$_POST['quantity'];
     $price=$_POST['price'];
-$query="update order_quantity set quantity=$quan where order_id=".$_GET['id'];
- $result=mysqli_query($connection,$query);
+$query="update order_product_mapping set qty=$quan where order_id=".$_GET['id'];
+ $result=mysqli_query($conn,$query);
     echo $query;
     $query="update order_master set total=$price where order_id=".$_GET['id'];
- $result=mysqli_query($connection,$query);
+ $result=mysqli_query($conn,$query);
 echo "done";
 header("Location: acceptorder.php");
 }
